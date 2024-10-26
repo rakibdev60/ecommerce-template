726e4faefc0226293dd762fcc3c42bd2cc44eb9d
MDashik-develop <MDashik.design.laptop@gmail.com>
2024-10-27 00:01:36 +0600

    updated

--- asset/style/js/component/header.js
+++ asset/style/js/component/header.js
@@ -10,7 +10,7 @@ function createHeader() {
                     <span>Call Us <br> 01000000000</span>
                 </section>
                 <div class=" md:h-[55px] w-1/3 flex bg-white border rounded-md overflow-hidden md:items-center">
-                    <input class="md:w-[90%] md:h-[100%] md:items-center bg-transparent md:pl-2 md:text-md" type="text" placeholder="Search heare..." />
+                    <input class="md:w-[90%] md:h-[100%] md:items-center bg-transparent md:pl-2 md:text-md" type="text" placeholder="search here.." />
                     <div class="flex md:w-[10%] md:h-full md:items-center md:p-2 bg-[orange]">
                         <svg class="fill-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                             <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/>
