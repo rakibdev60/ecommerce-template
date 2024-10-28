a5d9e1cf80cb8967da219956d10295be4e0cb86d
MDashik-develop <MDashik.design.laptop@gmail.com>
2024-10-28 14:51:50 +0600

    product svg convert icon and effect add

--- asset/style/js/component/features-products.js
+++ asset/style/js/component/features-products.js
@@ -5,14 +5,15 @@ function createFerfeaturedProduct() {
             <div class="featurs-product">
                 <div class="products" style="background-image: url('https://img.freepik.com/free-photo/organic-cosmetic-product-with-dreamy-aesthetic-fresh-background_23-2151382816.jpg');">
                     <div class="top">
+                    <button id="favorited"><i class="material-icons">favorite</i></button>
                     </div>
                     <div class="down">
                         <h1>This is safely usedfdddull product</h1>
                         <h2>$750.00</h2>
                         <section>
-                            <button><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M160 112c0-35.3 28.7-64 64-64s64 28.7 64 64l0 48-128 0 0-48zm-48 48l-64 0c-26.5 0-48 21.5-48 48L0 416c0 53 43 96 96 96l256 0c53 0 96-43 96-96l0-208c0-26.5-21.5-48-48-48l-64 0 0-48C336 50.1 285.9 0 224 0S112 50.1 112 112l0 48zm24 48a24 24 0 1 1 0 48 24 24 0 1 1 0-48zm152 24a24 24 0 1 1 48 0 24 24 0 1 1 -48 0z"/></svg></button>
-                            <button><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M0 24C0 10.7 10.7 0 24 0L69.5 0c22 0 41.5 12.8 50.6 32l411 0c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3l-288.5 0 5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5L488 336c13.3 0 24 10.7 24 24s-10.7 24-24 24l-288.3 0c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5L24 48C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/></svg></button>
-                            <button><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/></svg></button>
+                            <button><i class="material-icons">shopping_bag</i><h6>Buy Now</h6></button>
+                            <button><i class="material-icons">shopping_cart</i><h6>Add to Cart</h6></button>
+                            <button><i class="material-symbols-outlined">quick_reference_all</i><h6>See Details</h6></button>
                         </section>
                     </div>
                 </div>
