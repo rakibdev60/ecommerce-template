48273adb381ab883c9717b4297a251840c48798e
MDashik-develop <MDashik.design.laptop@gmail.com>
2024-10-28 15:14:25 +0600

    remove all fovuoreted id

--- asset/style/js/component/features-products.js
+++ asset/style/js/component/features-products.js
@@ -33,7 +33,7 @@ function createFerfeaturedProduct() {
                 </div>
                 <div class="products" style="background-image: url('https://img.freepik.com/free-photo/bottle-juice-with-fruit-it_1340-34396.jpg');">
                     <div class="top">
-                    <button id="favorited"><i class="material-icons">favorite</i></button>
+                    <button id=""><i class="material-icons">favorite</i></button>
                     </div>
                     <div class="down">
                         <h1>This is safely usedfdddull product</h1>
@@ -47,7 +47,7 @@ function createFerfeaturedProduct() {
                 </div>
                 <div class="products" style="background-image: url('https://cdn.pixabay.com/photo/2020/03/06/16/16/cornflakes-4907490_1280.jpg');">
                     <div class="top">
-                    <button id="favorited"><i class="material-icons">favorite</i></button>
+                    <button id=""><i class="material-icons">favorite</i></button>
                     </div>
                     <div class="down">
                         <h1>This is safely usedfdddull product</h1>
@@ -61,7 +61,7 @@ function createFerfeaturedProduct() {
                 </div>
                 <div class="products" style="background-image: url('https://d2v5dzhdg4zhx3.cloudfront.net/web-assets/images/storypages/primary/ProductShowcasesampleimages/JPEG/Product+Showcase-1.jpg');">
                     <div class="top">
-                    <button id="favorited"><i class="material-icons">favorite</i></button>
+                    <button id=""><i class="material-icons">favorite</i></button>
                     </div>
                     <div class="down">
                         <h1>This is safely usedfdddull product</h1>
@@ -75,7 +75,7 @@ function createFerfeaturedProduct() {
                 </div>
                 <div class="products" style="background-image: url('https://b2861582.smushcdn.com/2861582/wp-content/uploads/2023/02/splash-01-605-v1.png');">
                     <div class="top">
-                    <button id="favorited"><i class="material-icons">favorite</i></button>
+                    <button id=""><i class="material-icons">favorite</i></button>
                     </div>
                     <div class="down">
                         <h1>This is safely usedfdddull product</h1>
@@ -88,6 +88,34 @@ function createFerfeaturedProduct() {
                     </div>
                 </div>
                 <div class="products" style="background-image: url('https://img.freepik.com/premium-photo/ultra-realistic-orange-background-4k-hd-photo-product_1193781-21514.jpg');">
+                    <div class="top">
+                    <button id=""><i class="material-icons">favorite</i></button>
+                    </div>
+                    <div class="down">
+                        <h1>This is safely usedfdddull product</h1>
+                        <h2>$750.00</h2>
+                        <section>
+                            <button><i class="material-icons">shopping_bag</i><h6>Buy Now</h6></button>
+                            <button><i class="material-icons">shopping_cart</i><h6>Add to Cart</h6></button>
+                            <button><i class="material-symbols-outlined">quick_reference_all</i><h6>See Details</h6></button>
+                        </section>
+                    </div>
+                </div>
+                <div class="products" style="background-image: url('https://img.freepik.com/premium-photo/ultra-realistic-orange-background-4k-hd-photo-product_1193781-21514.jpg');">
+                    <div class="top">
+                    <button id=""><i class="material-icons">favorite</i></button>
+                    </div>
+                    <div class="down">
+                        <h1>This is safely usedfdddull product</h1>
+                        <h2>$750.00</h2>
+                        <section>
+                            <button><i class="material-icons">shopping_bag</i><h6>Buy Now</h6></button>
+                            <button><i class="material-icons">shopping_cart</i><h6>Add to Cart</h6></button>
+                            <button><i class="material-symbols-outlined">quick_reference_all</i><h6>See Details</h6></button>
+                        </section>
+                    </div>
+                </div>
+                <div class="" style="background-image: url('https://img.freepik.com/premium-photo/ultra-realistic-orange-background-4k-hd-photo-product_1193781-21514.jpg');">
                     <div class="top">
                     <button id="favorited"><i class="material-icons">favorite</i></button>
                     </div>
@@ -103,7 +131,7 @@ function createFerfeaturedProduct() {
                 </div>
                 <div class="products" style="background-image: url('https://img.freepik.com/premium-photo/ultra-realistic-orange-background-4k-hd-photo-product_1193781-21514.jpg');">
                     <div class="top">
-                    <button id="favorited"><i class="material-icons">favorite</i></button>
+                    <button id=""><i class="material-icons">favorite</i></button>
                     </div>
                     <div class="down">
                         <h1>This is safely usedfdddull product</h1>
@@ -117,7 +145,7 @@ function createFerfeaturedProduct() {
                 </div>
                 <div class="products" style="background-image: url('https://img.freepik.com/premium-photo/ultra-realistic-orange-background-4k-hd-photo-product_1193781-21514.jpg');">
                     <div class="top">
-                    <button id="favorited"><i class="material-icons">favorite</i></button>
+                    <button id=""><i class="material-icons">favorite</i></button>
                     </div>
                     <div class="down">
                         <h1>This is safely usedfdddull product</h1>
@@ -131,35 +159,7 @@ function createFerfeaturedProduct() {
                 </div>
                 <div class="products" style="background-image: url('https://img.freepik.com/premium-photo/ultra-realistic-orange-background-4k-hd-photo-product_1193781-21514.jpg');">
                     <div class="top">
-                    <button id="favorited"><i class="material-icons">favorite</i></button>
-                    </div>
-                    <div class="down">
-                        <h1>This is safely usedfdddull product</h1>
-                        <h2>$750.00</h2>
-                        <section>
-                            <button><i class="material-icons">shopping_bag</i><h6>Buy Now</h6></button>
-                            <button><i class="material-icons">shopping_cart</i><h6>Add to Cart</h6></button>
-                            <button><i class="material-symbols-outlined">quick_reference_all</i><h6>See Details</h6></button>
-                        </section>
-                    </div>
-                </div>
-                <div class="products" style="background-image: url('https://img.freepik.com/premium-photo/ultra-realistic-orange-background-4k-hd-photo-product_1193781-21514.jpg');">
-                    <div class="top">
-                    <button id="favorited"><i class="material-icons">favorite</i></button>
-                    </div>
-                    <div class="down">
-                        <h1>This is safely usedfdddull product</h1>
-                        <h2>$750.00</h2>
-                        <section>
-                            <button><i class="material-icons">shopping_bag</i><h6>Buy Now</h6></button>
-                            <button><i class="material-icons">shopping_cart</i><h6>Add to Cart</h6></button>
-                            <button><i class="material-symbols-outlined">quick_reference_all</i><h6>See Details</h6></button>
-                        </section>
-                    </div>
-                </div>
-                <div class="products" style="background-image: url('https://img.freepik.com/premium-photo/ultra-realistic-orange-background-4k-hd-photo-product_1193781-21514.jpg');">
-                    <div class="top">
-                    <button id="favorited"><i class="material-icons">favorite</i></button>
+                    <button id=""><i class="material-icons">favorite</i></button>
                     </div>
                     <div class="down">
                         <h1>This is safely usedfdddull product</h1>
