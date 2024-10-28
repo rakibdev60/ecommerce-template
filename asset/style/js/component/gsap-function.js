010e40d3f7bcdfb41c3f219511e7bb2a78624d42
MDashik-develop <MDashik.design.laptop@gmail.com>
2024-10-28 20:26:46 +0600

    locomotive delete and baner attaced fixed

--- asset/style/js/component/gsap-function.js
+++ asset/style/js/component/gsap-function.js
@@ -1,4 +1,4 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('html'),
-    smooth: true
-});
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('html'),
+//     smooth: true
+// });
