b6a51d978c6031f47a5ca6aba4b112caa6a117ad
MDashik-develop <MDashik.design.laptop@gmail.com>
2024-10-29 13:16:13 +0600

    footer add

--- asset/style/js/component/header.js
+++ asset/style/js/component/header.js
@@ -3,7 +3,7 @@ function createHeader() {
         <header class="flex flex-col bg-white pt-5 pb-2">
             <div class="flex justify-between gap-[70px] items-center p-[1px_10%]">
                 <div>
-                    <img class="w-[150px]" src="https://sajerbela.com/storage/app/public/company/2023-03-31-6426dc9918aaa.png" alt="">
+                    <img class="w-[150px] crsr-hover" src="https://sajerbela.com/storage/app/public/company/2023-03-31-6426dc9918aaa.png" alt="">
                 </div>
                 <section class="flex p-[15px] gap-2">
                     <svg class="h-[50px] border rounded p-[8px_8px]" fill="orange" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M16 64C16 28.7 44.7 0 80 0L304 0c35.3 0 64 28.7 64 64l0 384c0 35.3-28.7 64-64 64L80 512c-35.3 0-64-28.7-64-64L16 64zM224 448a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zM304 64L80 64l0 320 224 0 0-320z"/></svg>
